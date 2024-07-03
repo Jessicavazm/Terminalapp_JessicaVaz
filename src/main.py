6dff533bcf7adb53b06eb2d4308fe8b7c4f31844
Jessicavazm <jjessicavaz@gmail.com>
2024-07-03 18:21:25 +1000

    Added error handling on main function

--- src/main.py
+++ src/main.py
@@ -11,21 +11,27 @@ def main_function():
             4. View Notes
             5. Exit
             """)
+        try:
+            user_choice = input("Kindly select a number (1-5) to proceed: ")
 
-        user_choice = input("Kindly select a number (1-5) to proceed: ")
-        
-        if user_choice == "1":
-            add_note()
-        elif user_choice == "2":
-            edit_note()
-        elif user_choice == "3":
-            remove_note
-        elif user_choice == "4":
-            view_notes()
-        elif user_choice == "5":
-            print("Exiting the program")
-            break
-        else:
-            print("An error occurred, please try again.")
+            if user_choice == "1":
+                add_note()
+            elif user_choice == "2":
+                edit_note()
+            elif user_choice == "3":
+                remove_note()
+            elif user_choice == "4":
+                view_notes()
+            elif user_choice == "5":
+                print("Exiting the program")
+                break
+            else:
+                print("Invalid input, please choose an option from 1 to 5")
+        except KeyboardInterrupt:
+            print("Program interrupted by user, exiting in 3,2,1..")
+        except Exception as e:
+            print(f"Unexpected error occurred {e}")
 
+if __name__ == "__main__":
+    main_function()
 main_function()
